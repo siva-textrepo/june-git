304369611addc473606bd86c771e98cb0c452c87
siva <siva@gmail.com>
2022-07-19 10:27:40 -0700

    code

---
+++ MVP-Site-main/src/Feature/Navigation/platform/LayoutService/MainNavContentResolver.cs
@@ -0,0 +1,31 @@
+﻿using Mvp.Feature.Navigation.Services;
+using Sitecore.Diagnostics;
+using Sitecore.LayoutService.Configuration;
+using Sitecore.LayoutService.ItemRendering.ContentsResolvers;
+using Sitecore.Mvc.Presentation;
+
+namespace Mvp.Feature.Navigation.LayoutService
+{
+    public class MainNavContentResolver : RenderingContentsResolver
+    {
+        private readonly INavigationBuilder _navigationBuilder;
+        private readonly IItemTools _itemTools;
+
+        public MainNavContentResolver(IItemTools itemTools, INavigationBuilder navigationBuilder)
+        {
+            _itemTools = itemTools;
+            _navigationBuilder = navigationBuilder;
+        }
+
+        public override object ResolveContents(Rendering rendering, IRenderingConfiguration renderingConfig)
+        {
+            var rootItem = _itemTools.GetNavigationRootItem(GetContextItem(rendering, renderingConfig));
+            Assert.IsNotNull(rootItem, "Could not locate a navigation root item.");
+            return new
+            {
+                LogoSvgPath = rootItem[Templates.NavigationRootItem.Fields.LogoSvgPath],
+                Links = _navigationBuilder.GetNavigationLinks(rootItem)
+            };
+        }
+    }
+}
---
+++ MVP-Site-main/src/Feature/Navigation/rendering/Models/Footer.cs
@@ -0,0 +1,13 @@
+﻿using Sitecore.AspNet.RenderingEngine.Binding.Attributes;
+
+namespace Mvp.Feature.Navigation.Models
+{
+    public class Footer
+    {
+        [SitecoreComponentField]
+        public string CopyrightText { get; set; }
+        
+        [SitecoreComponentField]
+        public SocialLink[] SocialLinks { get; set; }
+    }
+}
